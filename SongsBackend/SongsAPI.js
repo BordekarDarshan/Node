aab8e82ecbfbb0b8f1584e021e57b8c182158246
Bordekar018 <darshanbordekar1819@gmail.com>
2020-01-18 17:55:19 +0530

    Update

--- SongsBackend/SongsAPI.js
+++ SongsBackend/SongsAPI.js
@@ -1,4 +1,5 @@
 const Express = require("express");
+const joi = require("@hapi/joi");
 const App = Express();
 let Port = process.env.NODE_ENV || 4800;
 App.use(Express.json());
@@ -9,7 +10,7 @@ App.listen(
   }, 2000)
 );
 
-let Songs = [];
+let Songs = [{ id: 1, name: "WayUp" }];
 
 //[GET]
 App.get("/Api/GetSongs", (req, res) => {
@@ -18,6 +19,10 @@ App.get("/Api/GetSongs", (req, res) => {
 
 //Add Data [POST].
 App.post("/Api/AddSong", (req, res) => {
+  let { error } = validation(req.body);
+  if (error) {
+    return res.send(error.details[0].message);
+  }
   let newSong = {
     id: Songs.length + 1,
     name: req.body.name
@@ -33,6 +38,12 @@ App.put("/Api/Update/:id", (req, res) => {
   if (!findSong) {
     return res.status(404).send({ message: "Invalid Course id" });
   }
+  let { error } = validation(req.body);
+  console.log(error);
+
+  if (error) {
+    return res.send(error.details[0].message);
+  }
   findSong.name = req.body.name;
   res.send({ Message: "Updated Successfully", Data: Songs });
 });
@@ -40,10 +51,19 @@ App.put("/Api/Update/:id", (req, res) => {
 //[DELETE]
 App.delete("/Api/delete/:id", (req, res) => {
   let FetchId = Songs.find(song => song.id == req.params.id);
-  if (!FetchId) {
+  console.log(FetchId);
+
+  if (FetchId) {
     res.status(404).send({ Data: "Data Not Found" });
   }
   let index = Songs.indexOf(FetchId);
   Songs.splice(index, 1);
   res.send({ Data: "Deleted" });
 });
+
+function validation(reqBody) {
+  let Schema = joi.object({
+    name: joi.string().min(2).max(15).required()
+  });
+  return Schema.validate(reqBody);
+}
