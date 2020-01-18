ac850c897513ba34bd3faeb4030dd4f52064020f
Bordekar018 <darshanbordekar1819@gmail.com>
2020-01-18 19:34:11 +0530

    CallBack

--- Asynchronous/CallBack.js
+++ Asynchronous/CallBack.js
@@ -28,7 +28,7 @@ function asyncCallBack(callback) {
 
 //CallBack Hell
 
-console.log("First");
+/* console.log("First");
 CH(parameterOfCallBack => {
   console.log(parameterOfCallBack);
   CH2(parameterOfCallBack, next => {
@@ -46,4 +46,25 @@ function CH2(prev, callback) {
   setTimeout(() => {
     callback("third");
   }, 2000);
+} */
+
+console.log("Please Wait.....");
+
+infoUser(parameterUserName => {
+  console.log(`Welcome ${parameterUserName}`);
+  cred(parameterUserName, password => {
+    console.log(`Hey ${parameterUserName} Your Password is ${password}`);
+  });
+});
+
+function infoUser(callBack) {
+  setTimeout(() => {
+    callBack("Darshan");
+  }, 3000);
+}
+
+function cred(name, callBack) {
+  setTimeout(() => {
+    callBack(1234);
+  }, 4000);
 }
